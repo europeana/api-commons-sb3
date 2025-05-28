37ceb243c6b7e3c3f8cdd1af73bb15cea0a23226
srishti <srishti.singh@europeana.eu>
2025-05-28 12:03:35 +0200

    EA-4210 refined Http async with pool and updated other methods

--- clientutils/src/main/java/eu/europeana/api/commons_sb3/http/AsyncHttpConnection.java
+++ clientutils/src/main/java/eu/europeana/api/commons_sb3/http/AsyncHttpConnection.java
@@ -2,44 +2,78 @@ package eu.europeana.api.commons_sb3.http;
 
 import eu.europeana.api.commons_sb3.auth.AuthenticationHandler;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.hc.client5.http.async.HttpAsyncClient;
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
-import org.apache.hc.client5.http.classic.methods.*;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.DefaultRedirectStrategy;
 import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.H2AsyncClientBuilder;
 import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManager;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
+import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.HttpHeaders;
-import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.hc.core5.http2.nio.pool.H2ConnPool;
+import org.apache.hc.core5.reactor.IOReactorConfig;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
-
+/**
+ * class that creates basic CloseableHttpAsyncClient.
+ * @author srishti singh
+ * @since 25 May 2025
+ */
 public class AsyncHttpConnection {
 
     private final CloseableHttpAsyncClient httpClient;
 
-   public AsyncHttpConnection(boolean withRedirect) {
+    /**
+     * Creates a AsyncHttpConnection without redirect strategy
+     */
+    public AsyncHttpConnection() {
+        this(false);
+    }
+
+
+    /**
+     * By default - DefaultRedirectStrategy is initialised if nothing is set.
+     * To create a connection without following redirects set 'redirectHandlingDisabled' to false
+     * @param withRedirect
+     */
+    public AsyncHttpConnection(boolean withRedirect) {
+        // creates a default pool of DEFAULT_MAX_CONNECTIONS_PER_ROUTE = 5 and DEFAULT_MAX_TOTAL_CONNECTIONS = 25
+        PoolingAsyncClientConnectionManager connPool = new PoolingAsyncClientConnectionManager();
         if (withRedirect) {
             RequestConfig requestConfig = RequestConfig.custom()
                     .setCircularRedirectsAllowed(true)
                     .build();
 
-            this.httpClient = HttpAsyncClients.customHttp2()
+            this.httpClient = HttpAsyncClients.custom()
+                    .setConnectionManager(connPool)
                     .setDefaultRequestConfig(requestConfig)
                     .setRedirectStrategy(new DefaultRedirectStrategy()).
                     build();
 
         } else {
-            this.httpClient = HttpAsyncClients.customHttp2().disableRedirectHandling().build();
+            this.httpClient = HttpAsyncClients.custom()
+                    .setConnectionManager(connPool)
+                    .disableRedirectHandling().build();
         }
-        httpClient.start();
     }
 
     /**
-     *This method makes GET request for given URL.
+     * we need to start the async client before using it;
+     * without that, we would get the following exception:
+     *   java.lang.IllegalStateException: Request cannot be executed; I/O reactor status: INACTIVE
+     */
+    public void start() {
+        this.httpClient.start();
+    }
+
+    /**
+     * This method makes GET request for given URL.
      * @param url
      * @param acceptHeaderValue
      * @param auth Authentication handler for the request
@@ -48,17 +82,13 @@ public class AsyncHttpConnection {
      */
 
     public SimpleHttpResponse get(String url, String acceptHeaderValue
-            , AuthenticationHandler auth) throws IOException, ExecutionException, InterruptedException {
+            , AuthenticationHandler auth) throws ExecutionException, InterruptedException {
         SimpleHttpRequest httpRequest = new SimpleHttpRequest("GET", url);
-        System.out.println("version of get  " +httpRequest.getVersion());
-
         if (StringUtils.isNotBlank(acceptHeaderValue)) {
             httpRequest.setHeader(HttpHeaders.ACCEPT, acceptHeaderValue);
         }
         if (auth!= null)         auth.setAuthorization(httpRequest);
-
-        Future<SimpleHttpResponse> response =  httpClient.execute(httpRequest, null);
-        return response.get();
+       return httpClient.execute(httpRequest, null).get();
     }
 
     /**
@@ -71,17 +101,17 @@ public class AsyncHttpConnection {
      * @return HttpResponseHandler that comprises response body as String and status code.
      * @throws IOException
      */
-    public HttpResponseHandler post(String url, String requestBody, String contentType
-            , AuthenticationHandler auth) throws IOException {
-        HttpPost post = new HttpPost(url);
-        if(StringUtils.isNotBlank(contentType)) {
-            addHeaders(post, HttpHeaders.CONTENT_TYPE, contentType);
+    public SimpleHttpResponse post(String url, String requestBody, String contentType
+            , AuthenticationHandler auth) throws ExecutionException, InterruptedException {
+        SimpleHttpRequest httpRequest = new SimpleHttpRequest("POST", url);
+        if (StringUtils.isNotBlank(contentType)) {
+            httpRequest.setHeader(HttpHeaders.CONTENT_TYPE, contentType);
         }
-        if (auth!= null)         auth.setAuthorization(post);
-        if(requestBody != null) {
-            post.setEntity(new StringEntity(requestBody));
+        if (auth!= null)         auth.setAuthorization(httpRequest);
+        if (requestBody != null) {
+            httpRequest.setBody(requestBody, ContentType.create(contentType));
         }
-        return executeHttpClient(post);
+        return httpClient.execute(httpRequest, null).get();
     }
 
 
@@ -95,43 +125,34 @@ public class AsyncHttpConnection {
      * @return HttpResponseHandler that comprises response body as String and status code.
      * @throws IOException
      */
-    public HttpResponseHandler put(String url, String jsonParamValue
-            , AuthenticationHandler auth) throws IOException {
-        HttpPut put = new HttpPut(url);
-        auth.setAuthorization(put);
-        put.setEntity(new StringEntity(jsonParamValue));
-
-        return executeHttpClient(put);
-
+    public SimpleHttpResponse put(String url, String jsonParamValue
+            , AuthenticationHandler auth) throws ExecutionException, InterruptedException {
+        SimpleHttpRequest httpRequest = new SimpleHttpRequest("PUT", url);
+        if (auth!= null)         auth.setAuthorization(httpRequest);
+        if (jsonParamValue != null) {
+            httpRequest.setBody(jsonParamValue, null);
+        }
+        return httpClient.execute(httpRequest, null).get();
     }
 
     /**
      * This method makes DELETE request for given identifier URL.
      *
      * @param url                       The identifier URL
-     * @param auth Authentication handler for the request
+     * @param auth Authentication handler for the request ( should not be null for deletion)
      * @return HttpResponseHandler that comprises response body as String and status code.
      * @throws IOException
      */
-    public HttpResponseHandler deleteURL(String url, AuthenticationHandler auth) throws IOException {
-        HttpDelete delete = new HttpDelete(url);
-        auth.setAuthorization(delete);
-        return executeHttpClient(delete);
+    public SimpleHttpResponse deleteURL(String url, AuthenticationHandler auth) throws ExecutionException, InterruptedException {
+        SimpleHttpRequest httpRequest = new SimpleHttpRequest("DELETE", url);
+        auth.setAuthorization(httpRequest);
+        return httpClient.execute(httpRequest, null).get();
     }
 
-
-    private <T extends HttpUriRequestBase> HttpResponseHandler executeHttpClient(T url) throws IOException {
-        HttpResponseHandler responseHandler = new HttpResponseHandler();
-        //httpClient.execute(SimpleHttpRequest.copy(url), null);
-        return responseHandler;
-    }
-
-    private <T extends HttpUriRequestBase> void addHeaders(T url, String headerName, String headerValue) {
-        if (StringUtils.isNotBlank(headerValue)) {
-            url.setHeader(headerName, headerValue);
-        }
-    }
-
+    /**
+     * Close the httpclient
+     * @throws IOException
+     */
     public void close() throws IOException {
         httpClient.close();
     }
